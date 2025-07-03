b65a9e3330ae6422e4a0ed81721c215f56ef6e99
Luka Guledani <lussskki@gmail.com>
2025-07-03 13:56:10 +0400

    Feature: implement menu in game

--- Silent-World/Main.cpp
+++ Silent-World/Main.cpp
@@ -1,7 +1,24 @@
 #include "Game.hpp"
+#include "Menu.hpp"
+#include <iostream>
 
 int main() {
-    Game game;
-    game.run();
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Silent World - Menu");
+
+    Menu menu(800, 600);
+    int selected = menu.run(window);
+
+    if (selected == 0) { // Play selected
+        Game game;
+        game.run();
+    }
+    else if (selected == 1) {
+        // You can later add settings/options screen here
+        std::cout << "Options selected (not yet implemented).\n";
+    }
+    else if (selected == 2) {
+        std::cout << "Exiting...\n";
+    }
+
     return 0;
 }
