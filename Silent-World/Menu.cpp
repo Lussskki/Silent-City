2a391399117e241b7b829fc27975512a78ee7308
Luka Guledani <lussskki@gmail.com>
2025-07-04 13:32:37 +0400

    Refactor: menu for readability

--- Silent-World/Menu.cpp
+++ Silent-World/Menu.cpp
@@ -1,15 +1,23 @@
-#include "Menu.hpp"
+﻿#include "Menu.hpp"
+#include "Options.hpp"
 
-Menu::Menu(float width, float height) {
+Menu::Menu(sf::RenderWindow& window) {
     font.loadFromFile("arial.ttf");
     std::vector<std::string> texts = { "Play", "Options", "Exit" };
 
+    backgroundTexture.loadFromFile("menu-jimi-hendrix.png");
+    backgroundSprite.setTexture(backgroundTexture);
+    backgroundSprite.setScale(800.f / backgroundTexture.getSize().x, 600.f / backgroundTexture.getSize().y);
+
     for (int i = 0; i < texts.size(); ++i) {
         sf::Text text(texts[i], font, 36);
-        text.setPosition(width / 2.f - 50, height / 2.f + i * 60 - 60);
-        text.setFillColor(i == 0 ? sf::Color::Red : sf::Color::White);
+        text.setPosition(50.f, 450.f + i * 50.f);
+        text.setFillColor(normalColor);
         options.push_back(text);
     }
+
+    // first selected one
+    options[selectedIndex].setFillColor(selectedColor);
 }
 
 int Menu::run(sf::RenderWindow& window) {
@@ -17,24 +25,55 @@ int Menu::run(sf::RenderWindow& window) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
-                return 2; // Exit selected
+                return 2;
 
             if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Up)
                     moveUp();
                 else if (event.key.code == sf::Keyboard::Down)
                     moveDown();
-                else if (event.key.code == sf::Keyboard::Enter)
-                    return selectedIndex;
+                else if (event.key.code == sf::Keyboard::Enter) {
+                    if (selectedIndex == 0) {
+                        return 0; // Play
+                    }
+                    else if (selectedIndex == 1) {
+                        // Open Options screen
+                        Options optionsMenu(window);
+                        optionsMenu.run(window);
+
+                        // Recreate window after fullscreen toggle
+                        window.close();
+                        if (Options::fullscreen) {
+                            window.create(sf::VideoMode::getDesktopMode(), "Silent World", sf::Style::Fullscreen);
+                        }
+                        else {
+                            window.create(sf::VideoMode(800, 600), "Silent World", sf::Style::Close);
+                        }
+
+                        // Reload background (needed after recreate)
+                        backgroundTexture.loadFromFile("menu-jimi-hendrix.png");
+                        backgroundSprite.setTexture(backgroundTexture);
+                        backgroundSprite.setScale(
+                            static_cast<float>(window.getSize().x) / backgroundTexture.getSize().x,
+                            static_cast<float>(window.getSize().y) / backgroundTexture.getSize().y
+                        );
+                    }
+                    else if (selectedIndex == 2) {
+                        return 2; // Exit
+                    }
+                }
             }
         }
+
         draw(window);
     }
+
     return 2;
 }
 
 void Menu::draw(sf::RenderWindow& window) {
     window.clear();
+    window.draw(backgroundSprite);
     for (const auto& text : options)
         window.draw(text);
     window.display();
@@ -42,16 +81,16 @@ void Menu::draw(sf::RenderWindow& window) {
 
 void Menu::moveUp() {
     if (selectedIndex > 0) {
-        options[selectedIndex].setFillColor(sf::Color::White);
+        options[selectedIndex].setFillColor(normalColor);
         selectedIndex--;
-        options[selectedIndex].setFillColor(sf::Color::Red);
+        options[selectedIndex].setFillColor(selectedColor);
     }
 }
 
 void Menu::moveDown() {
     if (selectedIndex < options.size() - 1) {
-        options[selectedIndex].setFillColor(sf::Color::White);
+        options[selectedIndex].setFillColor(normalColor);
         selectedIndex++;
-        options[selectedIndex].setFillColor(sf::Color::Red);
+        options[selectedIndex].setFillColor(selectedColor);
     }
 }
