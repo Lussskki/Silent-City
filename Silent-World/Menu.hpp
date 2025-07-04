fd1258859e097e16d1820b54f8adfd45407b106a
Luka Guledani <lussskki@gmail.com>
2025-07-04 13:34:03 +0400

    Header: add background textures and menu colors

--- Silent-World/Menu.hpp
+++ Silent-World/Menu.hpp
@@ -4,15 +4,24 @@
 
 class Menu {
 public:
-    Menu(float width, float height);
+    Menu(sf::RenderWindow&);
     int run(sf::RenderWindow& window);
     void draw(sf::RenderWindow& window);
     void moveUp();
     void moveDown();
-    int getSelectedIndex() const;
+    /*int getSelectedIndex() const;*/
 
 private:
     std::vector<sf::Text> options;
     int selectedIndex = 0;
     sf::Font font;
+
+    // menu - background photo
+    sf::Texture backgroundTexture;
+    sf::Sprite backgroundSprite;
+
+    // menu - color
+    sf::Color selectedColor = sf::Color(120, 220, 120);   // Acid Green
+    sf::Color normalColor = sf::Color(185, 100, 210);   // Psychedelic Plum
+
 };
