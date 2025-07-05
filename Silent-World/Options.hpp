853d202fd373e8bc4dcef5c2f98997b9f720773e
Luka Guledani <lussskki@gmail.com>
2025-07-05 17:29:03 +0400

    Header: add language headers

--- Silent-World/Options.hpp
+++ Silent-World/Options.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <string>
 
 class Options {
 public:
     static bool fullscreen;
+
     Options(sf::RenderWindow& window);
     int run(sf::RenderWindow& window); // returns when "Back" is selected
 
@@ -14,6 +16,7 @@ private:
     void moveDown();
     void toggleOption();
     void adjustVolume(int delta);
+    void updateItems();
 
     std::vector<sf::Text> items;
     sf::Font font;
@@ -24,12 +27,14 @@ private:
     sf::Sprite backgroundSprite;
 
     bool shouldExit = false;
-   
+    bool isEnglish;
+
+    std::string language;      // single declaration here, private
+
     int musicVolume = 50;
 
     sf::Color selectedColor = sf::Color(120, 220, 120); // Acid Green
     sf::Color normalColor = sf::Color(185, 100, 210);   // Psychedelic Plum
-    sf::RenderWindow* window;  // pointer to main window
 
-    void updateItems();
+    sf::RenderWindow* window;  // pointer to main window
 };
